cd8551ef1b885ab8c91c0d5cebb9a85083650e76
Dave Heseltine <daveomcdave@gmail.com>
2023-01-24 17:33:46 +0000

    Added JavaScript for highscores.html.

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,3 +4,46 @@ function goStartScreen() {
   window.location.href="index.html";
   document.querySelector("#start-screen").setAttribute("class", "visible");
 }
+// ## Function to clear the highscores and save to local:
+function highscoresClear() {
+  highscores = [];
+  highscores = JSON.stringify(highscores);
+  localStorage.setItem("highscores", highscores);
+}
+// ## Function to generate the list of Highscores, with a dynamic length:
+function highscoresGenerate() {
+  var highscoresLi = [];
+  for (var i = 0; i < highscores.length; i++) {
+    highscoresLi[i] = document.createElement("li");
+    document.querySelector("#highscores").appendChild(highscoresLi[i]);
+    highscoresLi[i].textContent = highscores[i].initials + " - " + highscores[i].score;
+    highscoresLi[i].style.fontWeight = "bold";
+  }
+}
+// ## Function to check for and upload highscores from local:
+function highscoresUpload() {
+  if (localStorage.getItem("highscores") !== null) {
+    highscores = JSON.parse(localStorage.getItem("highscores"));
+  }
+}
+
+
+// # Define Buttons
+// ## "Clear" button:
+document.querySelector("#clear").addEventListener("click", function(event) {
+  event.preventDefault();
+  highscoresClear();
+  location.reload();
+});
+// ## "Go Back" button:
+document.querySelector("a").addEventListener("click", function(event) {
+  event.preventDefault();
+  goStartScreen()
+});
+
+
+// # Initialise
+// ## Check for local data:
+highscoresUpload();
+// ## Generate "Highscores" element:
+highscoresGenerate();
